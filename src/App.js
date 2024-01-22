e887ebc07fadbc8ecd531874f5a680058c557df0
KJ Nam <paekma09@gmail.com>
2024-01-22 23:07:49 +0900

    chapter7_4

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import {useEffect, useState} from "react";
-
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    useEffect(() => {
-        fetch("http://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setLoading(false);
-            });
-    }, []);
-    return (
-        <div>
-            <h1>The Coins!({coins.length})</h1>
-            {loading ? <strong>Loading...</strong> : null}
-            <ul>
-                {coins.map((coin) => (
-                    <li>
-                        {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    return null;
 }
 
 export default App;
